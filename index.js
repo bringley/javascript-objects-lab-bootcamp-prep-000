b9faa3ccdd7065c77cc10962d397330941fa79ef
Brian Ringley <brian.ringley@wework.com>
2018-01-28 15:35:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 var recipes = {}
 
 //write function that takes object, key, val args and returns object with original and new key/val pairs
-function updateObjectWithKeyAndValue(object, key, value) {
+/*function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
-}
+}*/
 
 //same as above but return obj clone not orig obj
-function updateObjectWithKeyAndValue
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
