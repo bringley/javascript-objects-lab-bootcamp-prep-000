1236ca076d7cdb159862571b08b3833c814d875b
Brian Ringley <brian.ringley@wework.com>
2018-01-28 15:32:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+//define recipes Object
 var recipes = {}
+
+//write function that takes object, key, val args and returns object with original and new key/val pairs
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
